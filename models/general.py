5db739974a6e03bf6fd72db22d33cedeb14b46c7
ValRom28 <valentin.romanet@etu.univ-orleans.fr>
2025-11-20 13:50:02 +0100

    fix: correction circular import

--- models/general.py
+++ models/general.py
@@ -1,7 +1,7 @@
-from models.unit import Unit
+from __future__ import annotations
 
 class General:
-    def __init__(self, name: str, my_units: list[Unit]):
+    def __init__(self, name: str, my_units: list["Unit"]):
         self.name = name
         self.my_units = my_units
 
@@ -10,14 +10,14 @@ class General:
 
 
 class Daft(General):
-    def __init__(self, name: str, my_units: list[Unit]):
+    def __init__(self, name: str, my_units: list["Unit"]):
         super().__init__(name, my_units)
 
     def decide(self) -> None:
         pass
 
 class BrainDead(General):
-    def __init__(self, name: str, my_units: list[Unit]):
+    def __init__(self, name: str, my_units: list["Unit"]):
         super().__init__(name, my_units)
 
     def decide(self) -> None:
--- models/order.py
+++ models/order.py
@@ -1,14 +1,13 @@
-from models.unit import Unit
-
+from __future__ import annotations
 class Order:
-    def __init__(self, unit: Unit) -> None:
+    def __init__(self, unit: "Unit") -> None:
         self.unit = unit
 
     def action(self) -> None:
         pass
 
 class Move(Order):
-    def __init__(self, unit: Unit, x: int, y: int) -> None:
+    def __init__(self, unit: "Unit", x: int, y: int) -> None:
         super().__init__(unit)
         self.target_x = x
         self.target_y = y
@@ -17,7 +16,7 @@ class Move(Order):
         self.unit.move(self.target_x, self.target_y)
 
 class Attack(Order) :
-    def __init__(self, unit: Unit, target: Unit) -> None:
+    def __init__(self, unit: "Unit", target: "Unit") -> None:
         super().__init__(unit)
         self.target = target
 
@@ -25,7 +24,7 @@ class Attack(Order) :
         self.unit.attack_target(self.target)
 
 class Wait(Order):
-    def __init__(self, unit: Unit):
+    def __init__(self, unit: "Unit"):
         super().__init__(unit)
 
     def action(self) -> None:
