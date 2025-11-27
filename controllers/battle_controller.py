f259da7aa783832bdb8c57541d32b8769f6bb7a2
Anthony <95344837+Liwis779@users.noreply.github.com>
2025-11-27 16:31:02 +0100

    modify control mapping & some bugs (#28)
    * feat: map view for pygame
    
    - map visualisation in pygame view
    
    * feat: pikeman movement
    
    * feat: adding sprites orientation
    
    * bugfix : wrong map center
    
    * Feat : bunch of things
    
    - add : crossbowman units to the view
    - add : zoom and movement control for the view
    - Rework the loading asset function
    - add : text to differentiate units on the screen
    
    * feat : color army & knight
    
    - add differents color for each army
    - add Knight unit
    - improve GUI
    
    * zoom optimisation
    
    * bugfix : z axis error
    
    * bugfix : file case assets
    
    * feat : mapping control
    
    - save on F11
    - fullscreen on F10
    - ZOOM on mouse wheel
    
    * clean useless files

--- controllers/battle_controller.py
+++ controllers/battle_controller.py
@@ -22,10 +22,8 @@ class BattleController:
             try:
                 if key == keyboard.KeyCode.from_char('p'):
                     self.actions.put("pause")
-                # elif key == keyboard.KeyCode.from_char('s'):
-                #     self.actions.put("save")
-                # elif key == keyboard.Key.tab:
-                #     self.actions.put("snapshot")
+                elif key == keyboard.Key.f11:
+                    self.actions.put("save")
                 elif key == keyboard.Key.up:
                     self.actions.put("speed_up")
                 elif key == keyboard.Key.down:
