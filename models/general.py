96fe75cb34444746a32069477f1dd0f211457c68
Muhammed Puskullu <puskullumomo@gmail.com>
2025-11-27 16:06:54 +0100

    MomoIA

--- models/general.py
+++ models/general.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 import random
 
 from models.order import Attack, Move, Wait
-from models.unit import Unit
+from models.unit import *
 
 class General:
     def __init__(self, name: str, battle_model) -> None: # pyright: ignore[reportUndefinedVariable]
@@ -59,13 +59,70 @@ class AttackTestGeneral(General):
                 else:
                     unit.action = Wait(unit)
 
+class MomoIA(General):
+    def __init__(self, battle_model):
+        super().__init__("MomoIA", battle_model)
+
+    def decide(self):
+        enemy_units = self.battle_model.get_enemy_army(self)
+        n = len(enemy_units)
+        avg_x = sum(u.x for u in enemy_units) / n
+        avg_y = sum(u.y for u in enemy_units) / n
+        for unit in self.battle_model.get_army(self):
+                # Knights
+                if isinstance(unit, Knight) and isinstance(unit.action, Wait):
+                    nearest = min(enemy_units, key=lambda e: (e.x - unit.x)**2 + (e.y - unit.y)**2)
+                    dist2 = (nearest.x - unit.x)**2 + (nearest.y - unit.y)**2
+
+                    # Attaque si proche
+                    if dist2 <= 4: 
+                        unit.action = Attack(unit, nearest)
+                    else :
+                        dx = avg_x - unit.x
+                        dy = avg_y- unit.y
+                        distance = (dx*dx + dy*dy) ** 0.5
+                        unit.action = Move(unit, unit.x+dx / distance, unit.y+dy / distance)
+                else :
+                    unit.action = Wait(unit)
+
+                #Crossbowman
+                if isinstance(unit, Crossbowman):
+                    nearest = min(enemy_units, key=lambda e: (e.x - unit.x)**2 + (e.y - unit.y)**2)
+                    dist2 = (nearest.x - unit.x)**2 + (nearest.y - unit.y)**2
+                    # Attaque si proche
+                    if dist2 <= 4: 
+                        unit.action = Attack(unit, nearest)
+                    else :
+                        dx = avg_x - unit.x
+                        dy = avg_y- unit.y
+                        distance = (dx*dx + dy*dy) ** 0.5
+                        unit.action = Move(unit, unit.x+dx / distance, unit.y+dy / distance)
+                else :
+                    unit.action = Wait(unit)
+                
+                #Pikeman
+                if isinstance(unit, Pikeman) and isinstance(unit.action, Wait):
+                    nearest = min(enemy_units, key=lambda e: (e.x - unit.x)**2 + (e.y - unit.y)**2)
+                    dist2 = (nearest.x - unit.x)**2 + (nearest.y - unit.y)**2
+                    # Attaque si proche
+                    if dist2 <= 4: 
+                        unit.action = Attack(unit, nearest)
+                    else :
+                        dx = avg_x - unit.x
+                        dy = avg_y- unit.y
+                        distance = (dx*dx + dy*dy) ** 0.5
+                        unit.action = Move(unit, unit.x+dx / distance, unit.y+dy / distance)
+                else :
+                    unit.action = Wait(unit)
+
 
 def generalFactory(general_type: str, battle_model) -> General:
     general_classes = {
         "daft": Daft,
         "braindead": BrainDead,
         "movetest": MoveTestGeneral,
-        "attacktest": AttackTestGeneral
+        "attacktest": AttackTestGeneral,
+        "momoia": MomoIA
     }
     key = general_type.lower()
     if key in general_classes:
