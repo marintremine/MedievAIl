21ec230c6acd9490e190553c29865b8a825fbeb6
Anthony <95344837+Liwis779@users.noreply.github.com>
2025-12-06 17:11:29 +0100

    Feature/pygames view (#38)
    * feat: map view for pygame
    
    - map visualisation in pygame view
    
    * feat: pikeman movement
    
    * feat: adding sprites orientation
    
    * bugfix : wrong map center
    
    * Feat : bunch of things
    
    - add : crossbowman units to the view
    - add : zoom and movement control for the view
    - Rework the loading asset function
    - add : text to differentiate units on the screen
    
    * feat : color army & knight
    
    - add differents color for each army
    - add Knight unit
    - improve GUI
    
    * zoom optimisation
    
    * bugfix : z axis error
    
    * bugfix : file case assets
    
    * feat : mapping control
    
    - save on F11
    - fullscreen on F10
    - ZOOM on mouse wheel
    
    * clean useless files
    
    * bug fix pygames view crash on tournament mode

--- views/battle_view.py
+++ views/battle_view.py
@@ -5,6 +5,9 @@ class BattleView:
         self.model = model
         self.controller = controller
 
+    def load(self):
+        pass
+
     def render(self):
         pass
 
@@ -14,10 +17,31 @@ class BattleView:
     def cleanup(self):
         pass
 
-    def move_view(self, x, y):
+    def move_view_down(self):
         pass
 
-    def move_view_fast(self, x, y):
+    def move_view_up(self):
+        pass
+
+    def move_view_left(self):
+        pass
+
+    def move_view_right(self):
+        pass
+
+    def move_view_up_fast(self):
+        pass
+
+    def move_view_down_fast(self):
+        pass
+
+    def move_view_left_fast(self):
+        pass
+
+    def move_view_right_fast(self):
+        pass
+
+    def move_view_fast(self):
         pass
 
     def scroll_up(self):
