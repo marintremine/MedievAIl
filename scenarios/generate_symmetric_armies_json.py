58a0a6dba9a7dc9d7ed93310c5e1fea3195e04bd
Liwis779 <95344837+Liwis779@users.noreply.github.com>
2025-12-16 13:25:55 +0100

    obstacle view

--- scenarios/generate_symmetric_armies_json.py
+++ scenarios/generate_symmetric_armies_json.py
@@ -1,19 +1,26 @@
 import json
+import random
 
 # Paramètres de la map
-map_width = 120
-map_height = 120
+map_width = 200
+map_height = 200
 
 # Paramètres des unités
 num_crossbow = 80
 num_knight = 80
 num_pikeman = 80
+num_obstacle = 0
 
+# Générer obstacles
+obstacles = []
+obstacles += [{'type':'Bush', 'x':random.randint(0,map_width-1,), 'y':random.randint(0,map_height-1)} for _ in range(num_obstacle)]
+obstacles += [{'type':'Rock', 'x':random.randint(0,map_width-1,), 'y':random.randint(0,map_height-1)} for _ in range(num_obstacle)]
+obstacles += [{'type':'Tree', 'x':random.randint(0,map_width-1,), 'y':random.randint(0,map_height-1)} for _ in range(num_obstacle)]
 # Générer armée 1
 army1 = []
 army1 += [{'type':'crossbowman', 'x':i, 'y':0} for i in range(num_crossbow)]
-army1 += [{'type':'knight', 'x':i, 'y':1} for i in range(num_knight)]
-army1 += [{'type':'pikeman', 'x':i, 'y':2} for i in range(num_pikeman)]
+army1 += [{'type':'knight', 'x':i, 'y':10} for i in range(num_knight)]
+army1 += [{'type':'pikeman', 'x':i, 'y':16} for i in range(num_pikeman)]
 
 # Générer armée 2 (symétrique)
 army2 = []
@@ -25,12 +32,13 @@ army2 += [{'type':'pikeman', 'x':map_width-1-i, 'y':map_height-3} for i in range
 battlefield = {
     'map_width': map_width, 
     'map_height': map_height,
+    'obstacles': obstacles,
     'army1': army1,
     'army2': army2
 }
 
 # Écrire dans un fichier JSON
-with open('symmetric_armies.json', 'w') as f:
+with open('scenarios/symmetric_armies.json', 'w') as f:
     json.dump(battlefield, f, indent=2)
 
 print('Fichier symmetric_armies.json généré avec succès !')
