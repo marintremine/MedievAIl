f36f0cd4053c817d74f071aba4725f7bb19be7ab
LogicPulse <84001690+LogicPulsee@users.noreply.github.com>
2025-12-18 11:47:32 +0100

    IA_Global improvement (#59)
    Co-authored-by: augustin_orti <augustin.orti@insa-cvl.fr>
    Co-authored-by: Marin <64549344+marintremine@users.noreply.github.com>

--- models/general.py
+++ models/general.py
@@ -1,7 +1,4 @@
 from __future__ import annotations
-import random
-
-from models.order import Attack, Move, Wait, Defense
 from models.unit import *
 from enum import Enum
 
@@ -119,6 +116,7 @@ class IA_Global(General):
             self.tactic_group_units,
             self.tactic_avoid_hard_counters,
         ]
+        self.army_size = None
         self.formation_broken = False
         super().__init__("IA_Global", battle_model)
 
@@ -132,7 +130,7 @@ class IA_Global(General):
         """Si le puissance de l'unité est inférieuers a 30% il joint le point de rassemblement qui est la moyenne de tous ces alléis"""
         weight = 0
         order = None
-        if unit.hp > (unit.max_hp * 15 / 100):
+        if unit.hp > (unit.max_hp * 25 / 100):
             return weight, order
         allies = self.battle_model.get_army(self)
         if not allies:
@@ -189,7 +187,6 @@ class IA_Global(General):
             else:
                 weight = 90
                 order = Move(unit, target.x, target.y)
-
         return weight, order
 
     def tactic_attack_weakest(self, unit):
@@ -314,6 +311,10 @@ class IA_Global(General):
         return (x_avg, y_avg)
 
     def decide(self) -> None:
+        if self.army_size is None:
+            self.army_size = len(self.battle_model.get_army(self))
+        if (self.army_size * 20 // 100) >= self.army_size and self.army_size is not None:
+            self.strategy = self.Strategy.DEFENSIVE
         busy_list = set()
         army = self.battle_model.get_army(self)
         self.squad_attack(busy_list, army)
@@ -332,8 +333,6 @@ class IA_Global(General):
                             best_weight = weight
                             best_order = order
                             best_tactic_name = tactic
-                    # if best_weight > 20:
-                    #     print(f"[{unit.name}] -> {best_tactic_name} (Poids: {best_weight}) -> {best_order}")
                     unit.order = best_order
             case self.Strategy.DEFENSIVE:
                 for unit in self.battle_model.get_army(self):
